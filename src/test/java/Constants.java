a0616e0e9132c3d53a89524c75a7c931d732de85
KarinaMashkanova <karyna.mashkanava@gmail.com>
2022-02-08 01:59:50 +0300

    Revert "tests added"
    This reverts commit 7f04cca1329b2170e325a178d2b0c469cba169cb.

--- src/test/java/Constants.java
+++
@@ -1,5 +0,0 @@
-public class Constants {
-    public static final String ADD_REMOVE_ELEMETS_URL = "http://the-internet.herokuapp.com/add_remove_elements/";
-    public static final String DATA_TABLES_URL = "http://the-internet.herokuapp.com/tables";
-
-}
